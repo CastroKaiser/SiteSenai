47845cb29fa7b78ba2858e90ffa02a9a07303de8
castro <castro.nunes@aluno.senai.br>
2025-03-13 16:26:46 -0300

    Versão 1.3

---
+++ src/Cursos.jsx
@@ -0,0 +1,38 @@
+import './Cursos.css'
+
+function Cursos() {
+    return (
+        <>
+            <div class='card-father'>
+                <div class="c-card">
+                    <a href=""><img src="./src/assets/imagem.jpg" alt="imagem do card"/>
+                        <h2>Desenvolvimento de Sistemas</h2>
+                        <p>Data da Aplicação: 20/03/2025</p>
+                        <p>Data de Início: 20/03/2025</p>
+                        <p>Data de Térmico: 20/03/2025</p>
+                    </a>
+                </div>
+                <div class="c-card">
+                    <a href="">
+                        <img src="./src/assets/imagem.jpg" alt="imagem do card"/>
+                        <h2>Inteligência Artifícial</h2>
+                        <p>Data da Aplicação: 20/03/2025</p>
+                        <p>Data de Início: 20/03/2025</p>
+                        <p>Data de Térmico: 20/03/2025</p>
+                    </a>
+                </div>
+                <div class="c-card">
+                    <a href="">
+                        <img src="./src/assets/imagem.jpg" alt="imagem do card"/>
+                        <h2>Eletrotécnica</h2>
+                        <p>Data da Aplicação: 20/03/2025</p>
+                        <p>Data de Início: 20/03/2025</p>
+                        <p>Data de Térmico: 20/03/2025</p>
+                    </a>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Cursos
