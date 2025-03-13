19e770bc25eea95c7604d8c16762784b0a35818c
castro <castro.nunes@aluno.senai.br>
2025-03-13 14:52:19 -0300

    Inicializando Site do Senai

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div class='construction'>
+          <img src="./src/assets/logo-senai.png" alt="logo-senai" height='15%' width='15%' />
+        <div>
+            <ul id='layout'>
+              <li><a href="">Home</a></li>
+              <li><a href="">História</a></li>
+              <li><a href="">Contato</a></li>
+            </ul>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
