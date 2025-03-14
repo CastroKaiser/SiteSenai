70ba6945fd4b27d4d4798b641c61a3a8ff59d654
castro <castro.nunes@aluno.senai.br>
2025-03-14 15:51:00 -0300

    Versão 1.4

--- src/Cursos.jsx
+++ src/Cursos.jsx
@@ -6,32 +6,44 @@ function Cursos() {
     const EL = "Eletrotécnica";
     return (
         <>
-            <div class='card-father'>
-                <div class="c-card">
-                    <a href=""><img src="./src/assets/imagem.jpg" alt="imagem do card"/>
-                        <h2>Desenvolvimento de Sistemas</h2>
-                        <p>Data da Aplicação: 20/03/2025</p>
-                        <p>Data de Início: 20/03/2025</p>
-                        <p>Data de Térmico: 20/03/2025</p>
-                    </a>
-                </div>
-                <div class="c-card">
-                    <a href="">
-                        <img src="./src/assets/imagem.jpg" alt="imagem do card"/>
-                        <h2>Inteligência Artifícial</h2>
-                        <p>Data da Aplicação: 20/03/2025</p>
-                        <p>Data de Início: 20/03/2025</p>
-                        <p>Data de Térmico: 20/03/2025</p>
-                    </a>
-                </div>
-                <div class="c-card">
-                    <a href="">
-                        <img src="./src/assets/imagem.jpg" alt="imagem do card"/>
-                        <h2>Eletrotécnica</h2>
-                        <p>Data da Aplicação: 20/03/2025</p>
-                        <p>Data de Início: 20/03/2025</p>
-                        <p>Data de Térmico: 20/03/2025</p>
-                    </a>
+            <div class='container'>
+                <h1>Cursos oferecidos</h1>
+                <p class='personalizacao'>
+                    O Serviço Nacional de Aprendizagem Industrial (SENAI) é uma das principais
+                    instituições de ensino técnico e profissionalizante do Brasil, reconhecida
+                    por sua qualidade e foco na formação de profissionais para a indústria.
+                </p>
+                <p class='personalizacao'>
+                    Confira abaixo os cursos profissionalizantes oferecidos pela rede Senai.
+                </p>
+
+                <div class='card-father'>
+                    <div class="c-card">
+                        <a href=""><img src="./src/assets/DDS.jpg" alt="imagem do card" />
+                            <h2>Desenvolvimento de Sistemas</h2>
+                            <p>Data da Aplicação: 20/03/2025</p>
+                            <p>Data de Início: 20/03/2025</p>
+                            <p>Data de Térmico: 20/03/2025</p>
+                        </a>
+                    </div>
+                    <div class="c-card">
+                        <a href="">
+                            <img src="./src/assets/IA.jpg" alt="imagem do card" />
+                            <h2>Inteligência Artifícial</h2>
+                            <p>Data da Aplicação: 20/03/2025</p>
+                            <p>Data de Início: 20/03/2025</p>
+                            <p>Data de Térmico: 20/03/2025</p>
+                        </a>
+                    </div>
+                    <div class="c-card">
+                        <a href="">
+                            <img src="./src/assets/imagem.jpg" alt="imagem do card" />
+                            <h2>Eletrotécnica</h2>
+                            <p>Data da Aplicação: 20/03/2025</p>
+                            <p>Data de Início: 20/03/2025</p>
+                            <p>Data de Térmico: 20/03/2025</p>
+                        </a>
+                    </div>
                 </div>
             </div>
         </>
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -6,15 +6,15 @@ function Menu() {
     <>
       <div class='construction'>
           <img src="./src/assets/logo-senai.png" alt="logo-senai" height='15%' width='15%' />
-        <div>
-            <ul id='layout'>
-              <li><a href="">Home</a></li>
-              <li><a href="">História</a></li>
-              <li><a href="">Cursos</a></li>
-              <li><a href="">Contato</a></li>
-            </ul>
-        </div>
       </div>
+      <nav>
+          <ul id='layout'>
+            <li><a href="">Home</a></li>
+            <li><a href="">História</a></li>
+            <li><a href="">Cursos</a></li>
+            <li><a href="">Contato</a></li>
+          </ul>
+      </nav>
     </>
   )
 }
