265707550d2f52dffc9cc86e056dd5041c52f78f
castro <castro.nunes@aluno.senai.br>
2025-03-19 21:36:53 -0300

    Versão 2.5

--- src/conteudo-pagina/PagInfo.jsx
+++ src/conteudo-pagina/PagInfo.jsx
@@ -10,31 +10,33 @@ function PagInfo() {
 
     return(
         <div className="content">
-            <div className="formatation">
-                <h1>{cursoSelecionado.nome}</h1>
-                {/*<img src={ImgDDS} alt="Foto-curso" />*/}
-                <h2>Objetivo</h2>
-                <p>{cursoSelecionado.objetivo}</p>
-            </div>
-            
-            <div className="formatation">
-                <h2>Habilidades Adquiridas</h2>
-                <ul>
-                    As habilidades aprendidas no curso de {cursoSelecionado.nome}:
-                    {cursoSelecionado.habilidades.map((habilidade) => (
-                        <li key={habilidade.id}>{habilidade.nome}</li>
-                    ))}
-                </ul>
-            </div>
-            
-            <div className="formatation">
-                <h2>Áreas de Atuação</h2>
-                <ul>
-                    Os profissionais formados em {cursoSelecionado.nome} podem atuar em:
-                    {cursoSelecionado.atuacao.map((atua) => (
-                        <li key={atua.id}>{atua.nome}</li>
-                    ))}
-                </ul> 
+            <h1>{cursoSelecionado.nome}</h1>
+            <div className="organization">
+                <div className="style-list-none"> 
+                    {/*<img src={ImgDDS} alt="Foto-curso" />*/}
+                    <h2>Objetivo</h2>
+                    <p>{cursoSelecionado.objetivo}</p>
+                </div>
+                
+                <div className="style-list-none">
+                    <h2>Habilidades Adquiridas</h2>
+                    <ul>
+                        Habilidades aprendidas durante o curso:
+                        {cursoSelecionado.habilidades.map((habilidade) => (
+                            <li key={habilidade.id}>{habilidade.nome}</li>
+                        ))}
+                    </ul>
+                </div>
+                
+                <div className="style-list-none">
+                    <h2>Áreas de Atuação</h2>
+                    <ul>
+                        Os profissionais formados em {cursoSelecionado.nome} podem atuar em:
+                        {cursoSelecionado.atuacao.map((atua) => (
+                            <li key={atua.id}>{atua.nome}</li>
+                        ))}
+                    </ul> 
+                </div>
             </div>
         </div>
     )
