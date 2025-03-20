b8a209ce03fbc23622a5d34a0698561026cf7667
castro <castro.nunes@aluno.senai.br>
2025-03-20 00:01:14 -0300

    Versão aperfeiçoamento

--- src/conteudo-pagina/PagInfo.jsx
+++ src/conteudo-pagina/PagInfo.jsx
@@ -14,12 +14,12 @@ function PagInfo() {
             <div className="organization">
                 <div className="style-list-none"> 
                     {/*<img src={ImgDDS} alt="Foto-curso" />*/}
-                    <h2>Objetivo</h2>
+                    <h3>Objetivo</h3>
                     <p>{cursoSelecionado.objetivo}</p>
                 </div>
                 
                 <div className="style-list-none">
-                    <h2>Habilidades Adquiridas</h2>
+                    <h3>Habilidades Adquiridas</h3>
                     <ul>
                         Habilidades aprendidas durante o curso:
                         {cursoSelecionado.habilidades.map((habilidade) => (
@@ -29,13 +29,33 @@ function PagInfo() {
                 </div>
                 
                 <div className="style-list-none">
-                    <h2>Áreas de Atuação</h2>
+                    <h3>Áreas de Atuação</h3>
                     <ul>
                         Os profissionais formados em {cursoSelecionado.nome} podem atuar em:
                         {cursoSelecionado.atuacao.map((atua) => (
                             <li key={atua.id}>{atua.nome}</li>
                         ))}
                     </ul> 
+                </div>
+                <div className="formulario">
+                    <form action="">
+                        <div>
+                            <div className="form-space">
+                                <label htmlFor="">Nome Completo:</label>
+                                <input type="text" placeholder="Digite seu nome completo"/>
+                            </div>
+                            <div className="form-space">
+                                <label htmlFor="">Email:</label>
+                                <input type="text" placeholder="Digite seu email"/>
+                            </div>
+                            <div className="form-space">
+                                <label htmlFor="">Data de Nascimento:</label>
+                                <input type="date" name="" id="" placeholder="Insira sua idade"/>
+                            </div>
+                            
+                            <button>Enviar</button> 
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
